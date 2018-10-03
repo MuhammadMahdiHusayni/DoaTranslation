34c4273d6a0d06deef7b35c507e2dc3cb7351102
Muhammad Mahdi Husaayni <mmhusayni2@gmail.com>
2018-10-03 22:22:54 +0800

    Add project files.

---
+++ PersonalManager/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PersonalManager.MvcApplication" Language="C#" %>
---
+++ PersonalManager/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PersonalManager.Startup))]
+namespace PersonalManager
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
